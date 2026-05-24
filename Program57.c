b8a748b031e910df1fdb3a526d9ff1593c2575ee
Kiran Datir <datirkiran0@gmail.com>
2026-05-24 10:40:04 +0530

    initial commit

---
+++ Program57.c
@@ -0,0 +1,25 @@
+ // Input: 5
+// Output: 1 3 5 
+#include <stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt <= iNo; iCnt += 2)
+    {
+        printf("%d/t",iCnt);
+    }
+    printf("\n");
+
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number : \n");
+    scanf("%d",&iValue);
+
+    return 0;
+}
---
+++ Program63.c
@@ -0,0 +1,25 @@
+ // Input: 5
+// Output: -5 -3 -1 
+#include <stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = -iNo; iCnt < 0; iCnt = iCnt + 2)
+    {
+        printf("%d/t",iCnt);
+    }
+    printf("\n");
+
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number : \n");
+    scanf("%d",&iValue);
+
+    return 0;
+}
