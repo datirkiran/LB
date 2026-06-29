9b203d41ca240461f2fd2262f99c31e6ca3767ea
Kiran Datir <datirkiran0@gmail.com>
2026-06-29 16:33:23 +0530

    initial commit

---
+++ Program240.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+int main()
+{
+    char Arr[50] = {'\0'}; 
+   
+    printf("Enter string : \n");
+    scanf("%s", Arr);   // Issue
+
+    printf("Entered string is :%s\n",Arr);
+    
+     return 0;
+}
+
+   
