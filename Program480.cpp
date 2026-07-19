b4aae1330f71bbfe31889b7461a489db8fcebf80
Kiran Datir <datirkiran0@gmail.com>
2026-07-19 09:10:52 +0530

    initial commit

---
+++ Program480.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+template< class X>
+X Maximum(X No1, X No2, X No3)
+{
+    if(No1 > No2 && No1 > No3 )
+    {
+        return No1;
+    }
+    else if(No2 > No3 && No2 > No1 )
+    {
+        return No2;
+    }
+    else
+    {
+        return No3;
+    }
+}
+
+int main()
+{
+    cout<<Maximum(21.5f,11.2f, 51.5f)<<"\n";
+    cout<<Maximum(21.5,11.2,51.5)<<"\n";
+    cout<<Maximum(21,11, 51)<<"\n";
+
+    return 0;
+}
---
+++ Program486.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+class ArrayX
+{
+    public:
+        int *Arr;
+        int Size;
+
+        ArrayX(int No)
+        {
+            Size = No;
+            Arr = new int[Size];
+        }
+         ~ArrayX()
+        {
+           delete []Arr;  
+        }
+
+        void Accept()
+        {
+            int i = 0;
+            cout<<"Enter thr element : \n";
+
+            for(i = 0; i< Size; i++)
+            {
+                cin>>Arr[i];
+            }
+        }
+
+        void Display()
+        {
+            int i = 0;
+            cout<<"Element of the Array are : \n";
+
+            for(i = 0; i< Size; i++)
+            {
+                cout<<Arr[i]<<"\n";
+            }
+        }
+};
+
+int main()
+{
+    ArrayX aobj(5);
+
+    aobj.Accept();
+    aobj.Display();
+
+    return 0;
+}
